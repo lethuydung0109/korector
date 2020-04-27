5661aca016987d890024cf1a703b9191239c84c9
diagneAwa <mrsdiagne97@gmail.com>
2020-04-27 09:57:02 +0200

    Clean version code

---
+++ src/main/java/com/projet/korector/frontend/src/app/classes/section-name.spec.ts
@@ -0,0 +1,8 @@
+import { SectionName } from './section-name';
+
+describe('SectionName', () => {
+  it('should create an instance', () => {
+    expect(new SectionName(undefined)).toBeTruthy();
+  });
+});
+
---
+++ src/main/java/com/projet/korector/frontend/src/app/services.old/criteria-group.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import {CriteriaGroup} from "../classes/criteriaGroup";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CriteriaGroupService {
+
+  constructor() { }
+
+  getCriteriaGroupList() {
+    return undefined;
+  }
+
+  deleteCriteriaGroup(name: string) {
+
+  }
+
+  createCriteriaGroup(criteriaGroup: CriteriaGroup) {
+
+  }
+
+  getCriteriaGroup(id: number) {
+
+  }
+
+  updateCriteriaGroup(id: number, criteriaGroup: CriteriaGroup) {
+
+  }
+}
