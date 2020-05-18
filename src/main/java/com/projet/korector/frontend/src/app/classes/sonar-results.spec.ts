8acad27fe82a48d1029d04131ae3a84946438535
diagneAwa <mrsdiagne97@gmail.com>
2020-05-18 23:44:10 +0200

    Jenkins & Sonarqube integration + build + + get last build + notation

---
+++ src/main/java/com/projet/korector/frontend/src/app/classes/sonar-results.spec.ts
@@ -0,0 +1,7 @@
+import { SonarResults } from './sonar-results';
+
+describe('SonarResults', () => {
+  it('should create an instance', () => {
+    expect(new SonarResults()).toBeTruthy();
+  });
+});
