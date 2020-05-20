ad72a329a499d181e26b700cb1a5670db8abeda7
lethuydung <lethuydung0109@gmail.com>
2020-05-20 10:16:22 +0200

    Tomcat-compatible .war file packaging for deployment

--- src/main/java/com/projet/korector/KorectorApplication.java
+++ src/main/java/com/projet/korector/KorectorApplication.java
@@ -5,19 +5,22 @@ import com.projet.korector.jenkins.Jenkins;
 import org.apache.log4j.LogManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import java.util.logging.Logger;
 
 @SpringBootApplication
 @EnableConfigurationProperties(AppProperties.class)
-public class KorectorApplication {
+public class KorectorApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KorectorApplication.class, args);
-
 	}
 
-
-
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(KorectorApplication.class);
+	}
 }
