2210089e22fec764f967d0fb48aae9b3a2b3a9c5
lethuydung <lethuydung0109@gmail.com>
2020-05-24 11:53:04 +0200

    Config env variables for deployment

--- src/main/java/com/projet/korector/frontend/src/environments/environment.prod.ts
+++ src/main/java/com/projet/korector/frontend/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   api_url: 'http://34.69.54.57:8085/api',
   api_base_url: 'http://34.69.54.57:8085',
-  oauth2_redirect_uri: 'http://34.69.54.57:8888/korector/oauth2/redirect',
-  github_auth_uri: 'http://34.69.54.57:8085/oauth2/authorize/github?redirect_uri=http://34.69.54.57:8888/korector/oauth2/redirect'
+  oauth2_redirect_uri: 'http://34.69.54.57:4200/oauth2/redirect',
+  github_auth_uri: 'http://34.69.54.57:8085/oauth2/authorize/github?redirect_uri=http://34.69.54.57:4200/oauth2/redirect'
 };
